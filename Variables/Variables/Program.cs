5e6aa6face4aa30248c8a4b6e0e7e87699c28028
Paul Linck <paul.linck@gmail.com>
2016-07-18 15:25:20 -0400

    NEW

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int y;
+
+            x = 7;
+            y = x + 3;
+            Console.WriteLine(y);
+            Console.ReadLine();
+
+            Console.WriteLine("What is your name?");
+            Console.Write("What is your first name:");
+
+            String myFirstName;
+            myFirstName = Console.ReadLine();
+
+            String myLastName;
+            Console.Write("What is your last name:");
+
+            myLastName = Console.ReadLine();
+            Console.WriteLine("Hello, " + myFirstName + " " + myLastName);
+            Console.ReadLine();
+
+
+        }
+    }
+}
