9cbe38abfa4f54fc425f15103d35b8828ccd8a5c
Paul Linck <paul.linck@gmail.com>
2016-07-18 16:55:54 -0400

    char array

--- Arrays/Arrays/Program.cs
+++ Arrays/Arrays/Program.cs
@@ -11,6 +11,13 @@ namespace Arrays
         static void Main(string[] args)
         {
             int[] myNumbers = new int[5];
+            int[] myNewNumbers = new int[] { 4, 8, 12, 16, 55, 67, 88, 98 };  // Sets array size automatically
+            string[] myString = new string[] { "Paul", "Lynn", "Spencer", "Andrew" }; 
+
+            string myBiggie = "You cant always get what you want";
+
+            char[] charArray = myBiggie.ToCharArray();
+
 
             myNumbers[0] = 4;
             myNumbers[1] = 8;
@@ -18,11 +25,24 @@ namespace Arrays
             myNumbers[3] = 16;
             myNumbers[4] = 23;
 
+
+            Array.Reverse(charArray);
+            foreach (char myChar in charArray)
+            {
+                Console.Write(myChar);
+            }
+
             Console.WriteLine("Array Length is {0}", myNumbers.Length);
             for (int i = 0; i < 5; i++)
             {
                 Console.WriteLine("Array index {0} is {1}", i, myNumbers[i]);
             }
+            foreach (var stringName in myString)
+            {
+                Console.WriteLine("Name {0}", stringName);
+
+            }
+
             Console.ReadLine();
         }
     }
