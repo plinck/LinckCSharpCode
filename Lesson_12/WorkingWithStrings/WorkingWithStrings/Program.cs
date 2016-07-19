8a8f43e394caf524607a4a9b3cf2112900336d85
Paul Linck <paul.linck@gmail.com>
2016-07-19 17:53:18 -0400

    nrew

--- Lesson_12/WorkingWithStrings/WorkingWithStrings/Program.cs
+++ Lesson_12/WorkingWithStrings/WorkingWithStrings/Program.cs
@@ -15,40 +15,50 @@ namespace WorkingWithStrings
             //string myString = "Go to your c:\\ drive";
             //string myString = @"Go to your c:\ drive";
 
-            //string myString = String.Format("{1} = {0}", "First", "Second");
+            string myString = String.Format("{1} = {0}", "First", "Second");
+            
+            // Currency formatting
+            myString = string.Format("{0:C}", 123.45);
+            Console.WriteLine(myString);
 
-            //string myString = string.Format("{0:C}", 123.45);
-            //string myString = string.Format("{0:N}", 1234567890);
-            //string myString = string.Format("Percentage: {0:P}", .123);
-            //string myString = string.Format("Phone Number: {0:(###) ###-####}", 123456789012);
+            // Long number formatting with commas
+            myString = string.Format("{0:N}", 1234567890);
+            Console.WriteLine(myString);
 
-            //string myString = " That summer we took threes across the board  ";
+            // Show as precentage
+            myString = string.Format("Percentage: {0:P}", .123);
+            Console.WriteLine(myString);
 
-            //myString = myString.Substring(6, 14);
-            //myString = myString.ToUpper();
-            //myString = myString.Replace(" ", "--");
-            //myString = myString.Remove(6, 14);
+            // Custom Format
+            myString = string.Format("Phone Number: {0:(###) ###-####}", 1234567890);  // fills right to left and pushes left if too many
+            Console.WriteLine(myString);
 
-            /*
+            myString = " That summer we took threes across the board  ";
+            myString = myString.Substring(6, 14);
+            myString = myString.ToUpper();
+            myString = myString.Replace(" ", "--");
+            myString = myString.Remove(6, 4);
+            Console.WriteLine(myString);
+
+            myString = " That summer we took threes across the board  ";
             myString = String.Format("Length before: {0} -- Length after: {1}",
                 myString.Length, 
                 myString.Trim().Length);
-            */
+            Console.WriteLine(myString);
 
-            /*
-            string myString = "";
+
+            myString = "";
             for (int i = 0; i < 100; i++)
             {
                 myString += "--" + i.ToString();
             }
-            */
+            Console.WriteLine(myString);
 
-            StringBuilder myString = new StringBuilder();
-
+            StringBuilder myStringB = new StringBuilder();
             for (int i = 0; i < 100; i++)
             {
-                myString.Append("--");
-                myString.Append(i);
+                myStringB.Append("--");
+                myStringB.Append(i);
             }
 
             Console.WriteLine(myString);
