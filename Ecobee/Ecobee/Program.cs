421ea3d5fdbe3f76744daa405881ac9676dcb7c2
Paul Linck <paul.linck@gmail.com>
2018-03-07 15:06:47 -0500

    Small write update
    just to test how committing works

--- Ecobee/Ecobee/Program.cs
+++ Ecobee/Ecobee/Program.cs
@@ -6,7 +6,7 @@ namespace Ecobee
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World - get ecobee going!");
         }
     }
 }
